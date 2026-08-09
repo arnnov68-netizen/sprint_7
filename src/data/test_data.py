1045b2c573118939f2705b1b2744e47a201e060d
arnnov68-netizen <arnnov68@gmail.com>
2026-08-09 19:38:48 +0300

    Update test_data.py

--- src/data/test_data.py
+++ src/data/test_data.py
@@ -1,15 +1,15 @@
 class TestData:
     """Класс с тестовыми данными"""
 
-    # Данные для заказов - используем правильные имена параметров
+    # Данные для заказов
     ORDER_DATA = {
-        "first_name": "Иван",  # изменено с firstName на first_name
-        "last_name": "Петров",  # изменено с lastName на last_name
+        "first_name": "Иван",
+        "last_name": "Петров",
         "address": "Москва, ул. Пушкина, д. 10",
-        "metro_station": 1,  # изменено с metroStation на metro_station
+        "metro_station": 1,
         "phone": "+7 999 999-99-99",
-        "rent_time": 5,  # изменено с rentTime на rent_time
-        "delivery_date": "2024-12-31",  # изменено с deliveryDate на delivery_date
+        "rent_time": 5,
+        "delivery_date": "2024-12-31",
         "comment": "Позвонить за час"
     }
 
@@ -20,3 +20,17 @@ class TestData:
         ["BLACK", "GREY"],
         []
     ]
+
+    # Тексты сообщений об ошибках
+    ERROR_MESSAGES = {
+        "duplicate_login": "Этот логин уже используется",
+        "missing_fields": "Недостаточно данных для создания учетной записи",
+        "login_not_found": "Учетная запись не найдена",
+        "missing_login_data": "Недостаточно данных для входа",
+        "courier_not_found": "Курьера с таким id нет",
+        "courier_not_exists": "Курьера с таким id не существует",
+        "order_not_found": "Заказа с таким id нет",
+        "order_not_exists": "Заказа с таким id не существует",
+        "order_not_found_by_track": "Заказ не найден",
+        "insufficient_data_for_search": "Недостаточно данных для поиска"
+    }

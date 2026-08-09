ecb7fa88dcaae037a6a59f87dbe1d500e6ee1bb1
arnnov68-netizen <arnnov68@gmail.com>
2026-08-09 19:39:36 +0300

    Update courier_helper.py

--- src/helpers/courier_helper.py
+++ src/helpers/courier_helper.py
@@ -1,7 +1,5 @@
 import random
 import string
-import requests
-from src.api.endpoints import Endpoints
 
 
 def generate_random_string(length=10):
@@ -10,38 +8,15 @@ def generate_random_string(length=10):
     return ''.join(random.choice(letters) for i in range(length))
 
 
-def register_new_courier_and_return_login_password():
+def generate_courier_data():
     """
-    Регистрация нового курьера и возврат его данных
+    Генерация данных для нового курьера
 
     Returns:
-        list: [login, password, first_name] или пустой список при ошибке
+        tuple: (login, password, first_name)
     """
-    login_pass = []
-
     login = generate_random_string(10)
     password = generate_random_string(10)
     first_name = generate_random_string(10)
 
-    payload = {
-        "login": login,
-        "password": password,
-        "firstName": first_name
-    }
-
-    response = requests.post(Endpoints.COURIER_CREATE, data=payload)
-
-    if response.status_code == 201:
-        login_pass.extend([login, password, first_name])
-
-    return login_pass
-
-
-def get_courier_id(login, password):
-    """Получение ID курьера по логину и паролю"""
-    payload = {"login": login, "password": password}
-    response = requests.post(Endpoints.COURIER_LOGIN, data=payload)
-
-    if response.status_code == 200:
-        return response.json().get("id")
-    return None
+    return login, password, first_name

d0763e346ce20636963bb7734c13b6d947285052
arnnov68-netizen <arnnov68@gmail.com>
2026-08-09 19:40:50 +0300

    Update test_accept_order.py

--- tests/test_accept_order.py
+++ tests/test_accept_order.py
@@ -1,19 +1,19 @@
 import pytest
 import allure
+import requests
 from src.api.order_api import OrderAPI
 from src.api.courier_api import CourierAPI
 from src.data.test_data import TestData
-from src.helpers.courier_helper import register_new_courier_and_return_login_password, get_courier_id
+from src.helpers.courier_helper import generate_courier_data
 
 
 @allure.feature('Принятие заказа курьером')
 class TestAcceptOrder:
 
     @allure.title('Успешное принятие заказа')
-    def test_accept_order_success(self):
-        # Создаем курьера
-        login, password, first_name = register_new_courier_and_return_login_password()
-        courier_id = get_courier_id(login, password)
+    def test_accept_order_success(self, create_courier_and_get_id):
+        courier_data = create_courier_and_get_id
+        courier_id = courier_data["id"]
 
         # Создаем заказ
         order_data = TestData.ORDER_DATA.copy()
@@ -30,6 +30,9 @@ class TestAcceptOrder:
         assert accept_response.status_code == 200
         assert accept_response.json() == {"ok": True}
 
+        # Удаляем курьера
+        CourierAPI.delete_courier(courier_id)
+
     @allure.title('Принятие заказа без ID курьера')
     def test_accept_order_without_courier_id(self):
         # Создаем заказ
@@ -42,11 +45,9 @@ class TestAcceptOrder:
         order_id = order_response.json()["order"]["id"]
 
         # Принимаем заказ без ID курьера
-        import requests
         url = f"https://qa-scooter.praktikum-services.ru/api/v1/orders/accept/{order_id}"
         accept_response = requests.put(url)
 
-        # В зависимости от API может вернуть 400 или 500
         assert accept_response.status_code in [400, 500]
 
     @allure.title('Принятие заказа с неверным ID курьера')
@@ -64,27 +65,28 @@ class TestAcceptOrder:
         accept_response = OrderAPI.accept_order(order_id, 999999)
 
         assert accept_response.status_code == 404
-        # Проверяем оба варианта сообщения
-        assert "Курьера с таким id нет" in accept_response.text or "Курьера с таким id не существует" in accept_response.text
+        assert (TestData.ERROR_MESSAGES["courier_not_found"] in accept_response.text or
+                TestData.ERROR_MESSAGES["courier_not_exists"] in accept_response.text)
 
     @allure.title('Принятие заказа без ID заказа')
     def test_accept_order_without_order_id(self):
         # Пытаемся принять заказ без ID заказа
-        import requests
         url = "https://qa-scooter.praktikum-services.ru/api/v1/orders/accept/"
         response = requests.put(url)
 
         assert response.status_code in [404, 405]
 
     @allure.title('Принятие заказа с неверным ID заказа')
-    def test_accept_order_wrong_order_id(self):
-        # Создаем курьера
-        login, password, first_name = register_new_courier_and_return_login_password()
-        courier_id = get_courier_id(login, password)
+    def test_accept_order_wrong_order_id(self, create_courier_and_get_id):
+        courier_data = create_courier_and_get_id
+        courier_id = courier_data["id"]
 
         # Принимаем заказ с неверным ID
         accept_response = OrderAPI.accept_order(999999, courier_id)
 
         assert accept_response.status_code == 404
-        # Проверяем оба варианта сообщения
-        assert "Заказа с таким id нет" in accept_response.text or "Заказа с таким id не существует" in accept_response.text
+        assert (TestData.ERROR_MESSAGES["order_not_found"] in accept_response.text or
+                TestData.ERROR_MESSAGES["order_not_exists"] in accept_response.text)
+
+        # Удаляем курьера
+        CourierAPI.delete_courier(courier_id)

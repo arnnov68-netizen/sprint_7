a6dd0fd612c4d014e687d5fc2644ca12e63e0a9b
arnnov68-netizen <arnnov68@gmail.com>
2026-08-09 19:41:35 +0300

    Update test_create_courier.py

--- tests/test_create_courier.py
+++ tests/test_create_courier.py
@@ -1,70 +1,65 @@
 import pytest
 import allure
 from src.api.courier_api import CourierAPI
-from src.helpers.courier_helper import generate_random_string, register_new_courier_and_return_login_password
+from src.helpers.courier_helper import generate_random_string, generate_courier_data
+from src.data.test_data import TestData
 
 
 @allure.feature('Создание курьера')
 class TestCreateCourier:
 
     @allure.title('Создание курьера с валидными данными')
-    def test_create_courier_success(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
+    def test_create_courier_success(self, delete_courier_after_test):
+        login, password, first_name = generate_courier_data()
 
-        response = CourierAPI.create_courier(login, password, first_name)
+        response = delete_courier_after_test(login, password, first_name)
+        courier_id = CourierAPI.login_courier(login, password).json().get("id")
 
         assert response.status_code == 201
         assert response.json() == {"ok": True}
 
+        # Удаляем курьера
+        CourierAPI.delete_courier(courier_id)
+
     @allure.title('Создание двух одинаковых курьеров')
     def test_create_duplicate_courier(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
-        first_name = generate_random_string(10)
+        login, password, first_name = generate_courier_data()
 
         # Первое создание
         response1 = CourierAPI.create_courier(login, password, first_name)
         assert response1.status_code == 201
+        courier_id = CourierAPI.login_courier(login, password).json().get("id")
 
         # Второе создание с теми же данными
         response2 = CourierAPI.create_courier(login, password, first_name)
         assert response2.status_code == 409
-        assert "Этот логин уже используется" in response2.text
+        assert TestData.ERROR_MESSAGES["duplicate_login"] in response2.text
+
+        # Удаляем курьера
+        CourierAPI.delete_courier(courier_id)
 
     @allure.title('Создание курьера без обязательного поля')
-    @pytest.mark.parametrize('missing_field', ['login', 'password'])
-    def test_create_courier_missing_field(self, missing_field):
-        login = generate_random_string(10) if missing_field != 'login' else ''
-        password = generate_random_string(10) if missing_field != 'password' else ''
+    @pytest.mark.parametrize('login, password, expected_error', [
+        ('', generate_random_string(10), TestData.ERROR_MESSAGES["missing_fields"]),
+        (generate_random_string(10), '', TestData.ERROR_MESSAGES["missing_fields"])
+    ])
+    def test_create_courier_missing_field(self, login, password, expected_error):
         first_name = generate_random_string(10)
 
         response = CourierAPI.create_courier(login, password, first_name)
 
         assert response.status_code == 400
-        assert "Недостаточно данных для создания учетной записи" in response.text
+        assert expected_error in response.text
 
     @allure.title('Создание курьера без firstName (необязательное поле)')
-    def test_create_courier_without_first_name(self):
-        login = generate_random_string(10)
-        password = generate_random_string(10)
+    def test_create_courier_without_first_name(self, delete_courier_after_test):
+        login, password, _ = generate_courier_data()
 
-        response = CourierAPI.create_courier(login, password)
+        response = delete_courier_after_test(login, password, None)
+        courier_id = CourierAPI.login_courier(login, password).json().get("id")
 
         assert response.status_code == 201
         assert response.json() == {"ok": True}
 
-    @allure.title('Создание курьера с существующим логином')
-    def test_create_courier_existing_login(self):
-        # Создаем первого курьера
-        login = generate_random_string(10)
-        password1 = generate_random_string(10)
-        CourierAPI.create_courier(login, password1)
-
-        # Пытаемся создать второго с тем же логином
-        password2 = generate_random_string(10)
-        response = CourierAPI.create_courier(login, password2)
-
-        assert response.status_code == 409
-        assert "Этот логин уже используется" in response.text
+        # Удаляем курьера
+        CourierAPI.delete_courier(courier_id)
